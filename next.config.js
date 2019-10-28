b61fc8b68c4bcb3b11acd476fee2f819dfc132ef
cbdyzj <cbdyzj@jianzhao.org>
2019-10-28 19:37:11 +0800

    Amend

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-    webpack(config, { isServer }) {
-        if (!isServer) {
-            config.node = {
-                fs: 'empty'
-            }
-        }
-
+    webpack(config, option) {
         config.module.rules.push({
             test: /\.md$/,
             use: 'raw-loader',
