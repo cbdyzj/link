f09c67ac1ab04ea752d1e3f078f730d131c9f3b7
cbdyzj <cbdyzj@jianzhao.org>
2020-01-14 19:31:20 +0800

    Amend

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,14 +16,6 @@ function Index(props) {
                         <h3>文章列表 &rarr;</h3>
                         <p>一些文章。</p>
                     </a>
-                    <a href='https://nextjs.org/learn' className='card'>
-                        <h3>Next.js Learn &rarr;</h3>
-                        <p>Learn about Next.js by following an interactive tutorial!</p>
-                    </a>
-                    <a href='https://nextjs.org/docs' className='card'>
-                        <h3>Documentation &rarr;</h3>
-                        <p>Learn more about Next.js in the documentation.</p>
-                    </a>
                 </div>
             </div>
 
