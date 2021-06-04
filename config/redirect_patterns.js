c901dfde1a45057606ac9936e8bc698aec16ed5f
cbdyzj <cbdyzj@jianzhao.org>
2021-06-04 22:15:01 +0800

    Upgrade deps

--- config/redirect_patterns.js
+++ config/redirect_patterns.js
@@ -2,8 +2,5 @@ export default [
     {
         pattern: /^www.jianzhao.org$/,
         to: 'https://jianzhao.org',
-    }, {
-        pattern: /^payroll.jianzhao.org$/,
-        to: 'https://org-jianzhao-payroll.herokuapp.com',
-    },
+    }
 ]
